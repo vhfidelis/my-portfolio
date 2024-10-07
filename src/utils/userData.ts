c271d9b4be744f8ea9f4de004efa00beb566e0ff
unknown <victtorfidelis@gmail.com>
2024-10-07 16:34:18 -0300

    first commi9t

---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Victtor Fidelis",
+  githubUser: "vhfidelis",
+  linkedinUser: "victtorfidelis",
+  whatsappNumber: "+5591991942905",
+  emailUser: "victtorfidelis@gmail.com",
+  instagramUser: "vhf_ftw",
+  facebookUser: "",
+};
